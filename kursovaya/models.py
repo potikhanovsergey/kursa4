6b944e9694896c8600028465b5994a83c2286281
Sergey <potikhanovsergey@gmail.com>
2021-05-23 13:07:24 +0300

    BLOG posts

--- kursovaya/models.py
+++ kursovaya/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    author = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=50, null=True)
+    message = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.author + ' - ' + self.title
--- kursovaya/views.py
+++ kursovaya/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView, ListView, DetailView
 # Create your views here.
+from .models import Post
+
+class HomeTemplateView(TemplateView):
+    template_name = 'kursovaya/index.html'
+
+class AboutTemplateView(TemplateView):
+    template_name = 'kursovaya/about.html'
+
+class BlogTemplateView(ListView):
+    model = Post
+    template_name = 'kursovaya/blog.html'
+    context_object_name = 'posts'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'kursovaya/post_detail.html'
+
+class ContactsTemplateView(TemplateView):
+    template_name = 'kursovaya/contacts.html'
+
+
+
