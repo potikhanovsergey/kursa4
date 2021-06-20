fdda159001b49cd53931df18b1c7204cf98729a9
Sergey <potikhanovsergey@gmail.com>
2021-06-20 14:33:56 +0300

    'save'

--- pages/models.py
+++ pages/models.py
@@ -33,4 +33,35 @@ class Service(models.Model):
     img_title = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.title;
+        return self.title
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.CharField(max_length=50)
+
+
+
+class ClientService(models.Model):
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    service_id = models.ForeignKey(Service, on_delete=models.CASCADE)
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=False)
+    is_done = models.BooleanField(default=False)
+
+
+
+
+
